3078ce8b85807ad35fe62ae313042b31cc87252c
SaeedNajafi <snajafi@ualberta.ca>
2025-02-18 15:13:21 -0500

    save

---
+++ run_eval_command.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+eval "$(conda shell.bash hook)"
+conda activate llm-env
+
+export CUDA_HOME=$CONDA_PREFIX
+export NCCL_HOME=$CONDA_PREFIX
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
+
+run_name="llama3.2-1b-sftdatasetv3-sft-checkpoint-111000-offline-sft-lr-1.0e-6_checkpoint-3569_full_model"
+export CUDA_VISIBLE_DEVICES="0,1"
+NUM_GPUs=2
+
+date;pwd
+
+export MASTER_ADDR="$(hostname --fqdn)"
+export MASTER_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1])')"
+export RDVZ_ID=$RANDOM
+echo "RDZV Endpoint $MASTER_ADDR:$MASTER_PORT"
+
+# export TORCH_DISTRIBUTED_DEBUG=DETAIL
+# export NCCL_DEBUG=WARN
+# export NCCL_DEBUG_SUBSYS=WARN
+# export TORCH_CPP_LOG_LEVEL=INFO
+# export LOGLEVEL=INFO
+
+export WANDB_PROJECT="mmpo-project"
+export WANDB_MODE="offline"
+export ACCELERATE_LOG_LEVEL="info"
+
+LOG_DIR="training_logs"
+LOG_PATH="${LOG_DIR}/log_${run_name}.log"
+# Make logging directories.
+mkdir -p "${LOG_DIR}"
+echo "Placing logs in: ${LOG_DIR}"
+
+accelerate launch \
+    --multi-gpu \
+    --num_machines=1 \
+    --num_processes=$NUM_GPUs \
+    --machine_rank=0 \
+    --main_process_ip=$MASTER_ADDR \
+    --main_process_port=$MASTER_PORT \
+    --rdzv_backend=static \
+    lm_eval --model hf \
+    --model_args pretrained=/scratch/ssd004/scratch/snajafi/vector-backup/mmpo-experiments/llama3.2-1b-sftdatasetv3-sft-checkpoint-111000-offline-sft-lr-1.0e-6/checkpoint-3569_full_model,dtype=bfloat16 \
+    --tasks winogrande,arc_easy,piqa,boolq,hellaswag,openbookqa,arc_challenge,mmlu,gsm8k,mathqa,race,commonsense_qa \
+    --batch_size 4 \
+    --num_fewshot 3 \
+    --trust_remote_code \
+    --gen_kwargs max_new_tokens=1024,do_sample=False \
+    --output_path training_logs/lm_harness_output_${run_name} > ${LOG_PATH} 2>&1
