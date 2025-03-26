6b93d2715593bf09fa54b3cf7bd4bb80ec47fd80
SaeedNajafi <snajafi@ualberta.ca>
2025-03-26 15:37:56 +0000

    save

--- run_eval.sh
+++ run_eval.sh
@@ -8,10 +8,8 @@ export CUDA_HOME=$CONDA_PREFIX
 export NCCL_HOME=$CONDA_PREFIX
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 
-# run_name="Tinyllama_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_fuse_exps_0.2_data_lr_2e-5_checkpoint-6629"
-# run_name="Tinyllama_DTRNet_k_0.35_1.0KD+0.0CE_fuse_exps_0.2_data_lr_2e-5_checkpoint-2048"
-# run_name="Tinyllama_FUSED_DTRNet_k_0.35_DPO_fuse_exps_beta_0.05_checkpoint-2048"
-run_name="Tinyllama_Flex_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_fuse_exps_test_lr_2e-5_checkpoint-4244"
+
+run_name="Tinyllama_Flex_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_part_one_lr_2e-5_checkpoint-10000"
 export CUDA_VISIBLE_DEVICES="4,5,6,7"
 NUM_GPUs=4
 
@@ -47,11 +45,11 @@ accelerate launch \
     --main_process_port=$MASTER_PORT \
     --rdzv_backend=static \
     lm_eval --model hf \
-    --model_args pretrained=/work/saeed/dtrnet/Tinyllama_Flex_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_fuse_exps_test_lr_2e-5/checkpoint-4244,dtype=bfloat16,attn_implementation=eager \
+    --model_args pretrained=/work/saeed/dtrnet/Tinyllama_Flex_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_part_one_lr_2e-5/checkpoint-10000,dtype=bfloat16,attn_implementation=eager \
     --tasks winogrande,arc_easy,piqa,boolq,hellaswag,openbookqa,arc_challenge,mmlu \
     --batch_size 1 \
     --num_fewshot 0 \
     --trust_remote_code \
     --gen_kwargs max_new_tokens=1024,do_sample=False \
-    --output_path /work/saeed/dtrnet/Tinyllama_Flex_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_fuse_exps_test_lr_2e-5/checkpoint-4244/lm_harness_output > ${LOG_PATH} 2>&1
+    --output_path /work/saeed/dtrnet/Tinyllama_Flex_FUSED_DTRNet_k_0.35_1.0KD+0.0CE_part_one_lr_2e-5/checkpoint-10000/lm_harness_output > ${LOG_PATH} 2>&1
 
