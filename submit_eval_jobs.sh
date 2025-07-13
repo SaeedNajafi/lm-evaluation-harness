42d17d141ce7f18c41b84aede8318a9e5c870f53
SaeedNajafi <snajafi@ualberta.ca>
2025-07-13 13:00:03 -0400

    save

--- submit_eval_jobs.sh
+++ submit_eval_jobs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# DPO
-# epochs=(446 892 1338 1784 2225)
+# # DPO
+# epochs=(575 1150 1725 2300 2875)
 # logps=(yes no)
-# main_path="/home/saeednjf/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-dpo-v9"
+# main_path="/home/saeednjf/links/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-dpo-v10"
+
 
 # for logps_i in ${!logps[@]};
 #     do
@@ -11,70 +12,63 @@
 #         for epoch_i in ${!epochs[@]};
 #         do
 #             epoch=${epochs[$epoch_i]}
-#             model_dir=${main_path}/llama3.2-1b-offline-dpo-beta_0.01-lr_0.0005-avg_logps_${logp}-v9/checkpoint-${epoch}
-#             run_name=llama3.2-1b-offline-dpo-beta_0.01-lr_0.0005-avg_logps_${logp}-v9_checkpoint-${epoch}
+#             model_dir=${main_path}/llama3.2-1b-offline-dpo-beta_0.01-lr_0.0005-avg_logps_${logp}-v10/checkpoint-${epoch}_full_model
+#             run_name=llama3.2-1b-offline-dpo-beta_0.01-lr_0.0005-avg_logps_${logp}-v10_checkpoint-${epoch}
 #             sbatch run_eval_command.slrm MODEL_PATH=${model_dir} NAME=${run_name}
-#             sleep 2m
+#             # sleep 2m
 #     done
 # done
 
-# # SimPO
-# # epochs=(446 892 1338 1784 2225)
-# epochs=(446 1784)
-# logps=(yes no)
-# main_path="/home/saeednjf/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-simpo-v9"
-
-# for logps_i in ${!logps[@]};
-#     do
-#         logp=${logps[$logps_i]}
-#         for epoch_i in ${!epochs[@]};
-#         do
-#             epoch=${epochs[$epoch_i]}
-#             model_dir=${main_path}/llama3.2-1b-offline-simpo-beta_0.01-lr_0.0005-gamma-to-beta_1.0-avg_logps_${logp}-v9/checkpoint-${epoch}
-#             run_name=llama3.2-1b-offline-simpo-beta_0.01-lr_0.0005-gamma-to-beta_1.0-avg_logps_${logp}-v9_checkpoint-${epoch}
-#             sbatch run_eval_command.slrm MODEL_PATH=${model_dir} NAME=${run_name}
-#             sleep 3m
-#     done
-# done
-
-
-# # MMPO with entropy
-# epochs=(446 892 1338 1784 2225)
-# logps=(yes no)
-# main_path="/home/saeednjf/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-mmpo-v9"
-
-# for logps_i in ${!logps[@]};
-#     do
-#         logp=${logps[$logps_i]}
-#         for epoch_i in ${!epochs[@]};
-#         do
-#             epoch=${epochs[$epoch_i]}
-#             model_dir=${main_path}/llama3.2-1b-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v9-with-entropy/checkpoint-${epoch}
-#             run_name=llama3.2-1b-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v9-with-entropy_checkpoint-${epoch}
-#             sbatch run_eval_command.slrm MODEL_PATH=${model_dir} NAME=${run_name}
-#             sleep 3m
-#     done
-# done
-
-# MMPO
-epochs=(575 1150 1725 2300 2870)
+# SimPO
+epochs=(575 1150 1725 2300 2875)
 logps=(yes no)
-sizes=(360M 135M)
-main_path="/home/saeednjf/projects/def-afyshe-ab/saeednjf/checkpoints/smollm2/v2-runs"
+main_path="/home/saeednjf/links/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-simpo-v10"
 
-for size_i in ${!sizes[@]};
-do
-    size=${sizes[$size_i]}
-    for logps_i in ${!logps[@]};
+for logps_i in ${!logps[@]};
     do
         logp=${logps[$logps_i]}
         for epoch_i in ${!epochs[@]};
         do
             epoch=${epochs[$epoch_i]}
-            model_dir=${main_path}/smollm2-${size}-orca_bin_ultra-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v2-with-entropy/checkpoint-${epoch}
-            run_name=smollm2-${size}-orca_bin_ultra-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v2-with-entropy_checkpoint-${epoch}
+            model_dir=${main_path}/llama3.2-1b-offline-simpo-beta_0.01-lr_0.0005-gamma-to-beta_1.0-avg_logps_${logp}-v10/checkpoint-${epoch}_full_model
+            run_name=llama3.2-1b-offline-simpo-beta_0.01-lr_0.0005-gamma-to-beta_1.0-avg_logps_${logp}-v10_checkpoint-${epoch}
             sbatch run_eval_command.slrm MODEL_PATH=${model_dir} NAME=${run_name}
-            sleep 3m
-        done
+    done
+done
+
+
+# MMPO with entropy
+epochs=(575 1150 1725 2300 2875)
+logps=(yes no)
+main_path="/home/saeednjf/links/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-mmpo-v10"
+
+
+for logps_i in ${!logps[@]};
+    do
+        logp=${logps[$logps_i]}
+        for epoch_i in ${!epochs[@]};
+        do
+            epoch=${epochs[$epoch_i]}
+            model_dir=${main_path}/llama3.2-1b-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v10-with-entropy/checkpoint-${epoch}_full_model
+            run_name=llama3.2-1b-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v10-with-entropy_checkpoint-${epoch}
+            sbatch run_eval_command.slrm MODEL_PATH=${model_dir} NAME=${run_name}
+    done
+done
+
+# MMPO no entropy
+epochs=(575 1150 1725 2300 2875)
+logps=(yes no)
+main_path="/home/saeednjf/links/projects/def-afyshe-ab/saeednjf/checkpoints/llama-3.2-1b-mmpo-v10"
+
+
+for logps_i in ${!logps[@]};
+    do
+        logp=${logps[$logps_i]}
+        for epoch_i in ${!epochs[@]};
+        do
+            epoch=${epochs[$epoch_i]}
+            model_dir=${main_path}/llama3.2-1b-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v10-no-entropy/checkpoint-${epoch}_full_model
+            run_name=llama3.2-1b-offline-mmpo-beta_0.01-lr_0.0005-reward_eps_0.9-avg_logps_${logp}-v10-no-entropy_checkpoint-${epoch}
+            sbatch run_eval_command.slrm MODEL_PATH=${model_dir} NAME=${run_name}
     done
 done
